fb961dd91a643a6ef7baf9bd068017199435cc6e
Jerick Pizarra <jerick.pizarra@gmail.com>
2022-08-01 02:04:34 +0400

    'jerickpizarra'

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(express.static(__dirname + "/dist"))
+// app.use(express.static(__dirname + "/dist"))
 
 app.use('/api/user', require('./backend/routes/userRoutes'))
 app.use('/api/employee', require('./backend/routes/employeeRoutes'))
