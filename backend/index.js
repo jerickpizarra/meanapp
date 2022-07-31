0f83b95cc922c70f46cd1783d4293ba660c5912e
Jerick Pizarra <jerick.pizarra@gmail.com>
2022-08-01 01:06:28 +0400

    'herokuu123'

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,13 @@ const cors = require('cors')
 
 connectDB();
 
+const module = require('path');
+
+  app.use(express.static('public'));
+  app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public/index.html'));
+})
+
 const app = express()
 app.use(cors())
 
