e34bab1fb6f098b84b9a22e1d8251ee605fe7b6d
Jerick Pizarra <jerick.pizarra@gmail.com>
2022-08-01 03:02:46 +0400

    'jerickpizarra'

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ app.use(express.urlencoded({extended: false}))
 
 // app.use(express.static(__dirname + "/dist"))
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public/index.html'));
+    res.sendFile(path.join(__dirname,'./public/index.html'));
 })
 
 app.use('/api/user', require('./backend/routes/userRoutes'))
