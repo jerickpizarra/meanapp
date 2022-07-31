194fbebc50628d0b7ce0b2662c5c152aba6101d9
Jerick Pizarra <jerick.pizarra@gmail.com>
2022-08-01 02:36:55 +0400

    'jerickpizarra'

--- index.js
+++ index.js
@@ -15,10 +15,18 @@ app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+const module = require('path');
+
 // app.use(express.static(__dirname + "/dist"))
 
 app.use('/api/user', require('./backend/routes/userRoutes'))
 app.use('/api/employee', require('./backend/routes/employeeRoutes'))
+
+app.use(express.static('public'));
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public/index.html'));
+})
 
 app.use(errorHandler)
 app.listen(port, () => console.log(`Server started on port ${port}`))
