bc1383a144c3693a57519d75ff721e8f214fd16d
Jerick Pizarra <jerick.pizarra@gmail.com>
2022-08-01 01:18:03 +0400

    'herokupublish'

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
-const {errorHandler} = require('./middleware/errorMiddleware')
-const {connectDB} = require('./config/db')
+const {errorHandler} = require('./backend/middleware/errorMiddleware')
+const {connectDB} = require('./backend/config/db')
 const port = process.env.PORT || 5000
 const cors = require('cors')
 
 
 connectDB();
 
-const module = require('path');
-
-  app.use(express.static('public'));
-  app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public/index.html'));
-})
 
 const app = express()
 app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(express.static(__dirname + "/dist"))
 
-app.use('/api/user', require('./routes/userRoutes'))
-app.use('/api/employee', require('./routes/employeeRoutes'))
+app.use('/api/user', require('./backend/routes/userRoutes'))
+app.use('/api/employee', require('./backend/routes/employeeRoutes'))
 
 app.use(errorHandler)
 app.listen(port, () => console.log(`Server started on port ${port}`))
