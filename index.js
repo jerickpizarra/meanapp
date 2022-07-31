4b2b2d47bd55f43aed00aaa1c166f4c550047110
Jerick Pizarra <jerick.pizarra@gmail.com>
2022-08-01 02:46:41 +0400

    jerickpizarra

--- index.js
+++ index.js
@@ -20,13 +20,13 @@ app.use(express.urlencoded({extended: false}))
 
 // app.use(express.static(__dirname + "/dist"))
 
-app.use('/api/user', require('./backend/routes/userRoutes'))
-app.use('/api/employee', require('./backend/routes/employeeRoutes'))
-
 app.use(express.static('public'));
 app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'public/index.html'));
 })
+
+app.use('/api/user', require('./backend/routes/userRoutes'))
+app.use('/api/employee', require('./backend/routes/employeeRoutes'))
 
 app.use(errorHandler)
 app.listen(port, () => console.log(`Server started on port ${port}`))
